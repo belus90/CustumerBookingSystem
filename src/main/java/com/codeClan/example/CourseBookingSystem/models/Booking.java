ed417cc0af740b3530698bab79ae8b29aaf48e9c
belus90 <belus90@gmail.com>
2022-05-26 10:56:24 +0100

    booking table done

--- src/main/java/com/codeClan/example/CourseBookingSystem/models/Booking.java
+++ src/main/java/com/codeClan/example/CourseBookingSystem/models/Booking.java
@@ -1,4 +1,43 @@
 package com.codeClan.example.CourseBookingSystem.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    private String date;
+
+//    private Course course;
+//
+//    private Customer customer;
+
+
+    public Booking(String date) {
+        this.date = date;
+    }
+
+    public Booking() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/codeClan/example/CourseBookingSystem/repository/BookingRepository.java
+++ src/main/java/com/codeClan/example/CourseBookingSystem/repository/BookingRepository.java
@@ -1,4 +1,7 @@
 package com.codeClan.example.CourseBookingSystem.repository;
 
-public interface BookingRepository {
+import com.codeClan.example.CourseBookingSystem.models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
